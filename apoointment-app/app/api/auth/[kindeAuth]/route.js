fc32316a8b7b4bc958e0648477477670defcd0d9
boumedine-mohammed-dev <mohammed.boumedine@outlook.com>
2025-08-21 22:50:29 +0100

    FIX: vercel-specific cookie settings

--- apoointment-app/app/api/auth/[kindeAuth]/route.js
+++ apoointment-app/app/api/auth/[kindeAuth]/route.js
@@ -1,15 +1,2 @@
-// app/api/auth/[kinde_auth]/route.js
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export const GET = handleAuth({
-    authRouter: {
-        cookieOptions: {
-            secure: true,
-            sameSite: 'none',
-            domain: '.vercel.app', // 🔥 هذا هو المفتاح!
-            path: '/',
-            httpOnly: true,
-            maxAge: 60 * 10 // 10 دقائق
-        }
-    }
-});
+export const GET = handleAuth();
